6999da189d5b58a6ceecf941d0f38e722ee55bd5
César Alberca <cesalberca@gmail.com>
2022-04-05 12:02:12 +0200

    Render text

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world')
+import P5 from 'p5'
+
+function draw(p5: P5) {
+  p5.setup = () => {
+    const CANVAS_HEIGHT = p5.windowHeight
+    const CANVAS_WIDTH = p5.windowWidth
+    p5.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    p5.colorMode(p5.HSB, 100)
+
+    p5.background(10)
+
+    p5.textSize(46)
+    p5.fill(100)
+    p5.text('Autentia', CANVAS_HEIGHT / 2 - 80, CANVAS_WIDTH / 2 - 16).textAlign('center')
+  }
+}
+
+new P5(draw)
